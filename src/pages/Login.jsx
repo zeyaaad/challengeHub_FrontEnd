f0e80cf2fa2656c89ed26e751ce4ca22c4789a33
Zeyad Mohamed <114029186+zeyaaad@users.noreply.github.com>
2024-10-24 00:41:47 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -64,7 +64,7 @@ export default function Login() {
     setLoader(false);
   }
   return (
-  Loader&&<Loading/>
+    {Loader&&<Loading/>}
     <div>
       <div className="loginform text-white">
         <h2>Login now:</h2>
